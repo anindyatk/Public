56affe34416f888badb36f00b39ba509f4bb53ce
anindyatk <anindya.kurniawan@live.com>
2022-04-19 17:11:49 +0700

    Init

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace BasicAlgorithm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            bool validInput = false;
+            Console.Write("Please input n (Range 1-100): ");
+            string inputtedValue = Console.ReadLine();
+
+            if (int.TryParse(inputtedValue, out n))
+            {
+                validInput = true;
+            }
+
+            if (validInput && n > 0 && n <= 100)
+            {
+                for (int i = 0; i < n; i++)
+                {
+                    Console.Write("#".PadLeft(n - i));
+                    Console.WriteLine("".PadRight(i, '#'));
+                }
+                Console.WriteLine("Done!");
+            }
+            else
+            {
+                Console.WriteLine("Wrong input!\nInputted value should be integer\nRange 1-100\nPlease press anykey to exit...");
+                Console.ReadKey();                  
+            }
+
+        }
+    }
+}
---
+++ com.anindya.devtest/Models/CustomerContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace com.anindya.devtest.Models
+{
+    public class CustomerContext : DbContext
+    {
+        public CustomerContext(DbContextOptions<CustomerContext> options)
+                : base(options)
+        {
+        }
+
+        public DbSet<Customer> Customers { get; set; } = null!;
+    }
+}
